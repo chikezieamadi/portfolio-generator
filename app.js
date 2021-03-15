a5d302be731e6ef903791a745c967ac8409e8e29
Chikezie Amadi <chikezieamadi@gmail.com>
2021-03-15 09:56:06 -0600

    update inquirer

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+inquirer
+.prompt([{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name',
+}])
+.then(answers=>console.log(answers));
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', generatePage(name, github), err =>{
+//     if (err) throw err (err);
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
+
 // const generatePage = () => 'Name: Jane, Github: janehub';
 // console.log(generatePage());
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
-const [name, github] = profileDataArgs;
-
+// const [name, github] = profileDataArgs;
 
 // console.log(name, github);
 // console.log(generatePage(name, github));
-
-fs.writeFile('./index.html', generatePage(name, github), err =>{
-    if (err) throw err (err);
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
